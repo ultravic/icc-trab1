8e513d83fde4ac2c125edc33517313f4f1aef971
Pedro Moreira <plusmoreira@gmail.com>
2017-11-18 18:28:09 -0200

    correction

--- perf_measure.sh
+++ perf_measure.sh
@@ -7,13 +7,13 @@ for f in {trab1,trab2}; do
     make -B;
     for i in {32,33,64,65,128,129,256,257,512,1000,200}; do
         ./invmat -r $i -o ../measurement/$(f)_output.txt -i 10 } ;
-        {echo "#####\n##### $f\n#####\n" && likwid-perfcter -C 0 -g CACHE ./invmat -r $i -o /dev/null -i 10 } >>  ../measurement/cache.txt;
-        {echo "#####\n##### $f\n#####\n" && likwid-perfcter -C 0 -g MEM ./invmat -r $i -o /dev/null -i 10 } >>  ../measurement/mem.txt;
-        {echo "#####\n##### $f\n#####\n" && likwid-perfcter -C 0 -g FLOPS ./invmat -r $i -o /dev/null -i 10 } >>  ../measurement/flops.txt;
+        {echo "#####\n##### $f\n#####\n" && likwid-perfcter -C 0 -g CACHE -m ./invmat -r $i -o /dev/null -i 10 } >>  ../measurement/cache.txt;
+        {echo "#####\n##### $f\n#####\n" && likwid-perfcter -C 0 -g MEM -m ./invmat -r $i -o /dev/null -i 10 } >>  ../measurement/mem.txt;
+        {echo "#####\n##### $f\n#####\n" && likwid-perfcter -C 0 -g FLOPS -m ./invmat -r $i -o /dev/null -i 10 } >>  ../measurement/flops.txt;
     done
     cd ..;
 done
 cd trab2;
 for i in {32,33,64,65,128,129,256,257,512,1000,200}; do
-    {echo "#####\n##### trab2\n#####\n" && likwid-perfcter -C 0 -g FLOPS_AVX ./invmat -r $i -o /dev/null -i 10 } >/>  ../measurement/flops_avx.txt;
+    {echo "#####\n##### trab2\n#####\n" && likwid-perfcter -C 0 -g FLOPS_AVX -m ./invmat -r $i -o /dev/null -i 10 } >/>  ../measurement/flops_avx.txt;
 done
